ded50f2c59e4bb99eda38d9959f571a5af97dedc
jimmy <jimmylim0823@yonsei.ac.kr>
2024-04-01 13:04:43 +0900

    am strategy

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,16 @@
 from datamodel import OrderDepth, TradingState, Order
 
 
+class StrategyAmethysts:
+    FAIR_VALUE = 10000
+    POSITION_LIMIT = 20
+
+    def __init__(self, order_depth: OrderDepth):
+        self.bids = order_depth.buy_orders
+        self.asks = order_depth.sell_orders
+
+
+
 class Trader:
     @staticmethod
     def run(state: TradingState):
