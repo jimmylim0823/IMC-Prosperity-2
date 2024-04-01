fcbee545dcdcf3c40e318b166647093e6250af37
jimmy <jimmylim0823@yonsei.ac.kr>
2024-04-01 13:20:05 +0900

    am scratch

--- tutorial.py
+++ tutorial.py
@@ -1,14 +1,27 @@
-from datamodel import OrderDepth, TradingState, Order
+from datamodel import OrderDepth, TradingState, Position, Order
+from typing import List
 
 
 class StrategyAmethysts:
+    SYMBOL = "AMETHYSTS"
     FAIR_VALUE = 10000
     POSITION_LIMIT = 20
 
-    def __init__(self, order_depth: OrderDepth):
+    def __init__(self, order_depth: OrderDepth, position: Position):
         self.bids = order_depth.buy_orders
         self.asks = order_depth.sell_orders
+        self.position = position
 
+    def scratch_under_valued(self):
+        # scratch any par or under-valued order
+        orders: List[Order] = []
+        for bid_price, bid_amount in enumerate(self.bids):
+            if bid_price >= self.FAIR_VALUE:
+                orders.append(Order(self.SYMBOL, bid_price, -bid_amount))
+        for ask_price, ask_amount in enumerate(self.asks):
+            if ask_price >= self.FAIR_VALUE:
+                orders.append(Order(self.SYMBOL, ask_price, -ask_amount))
+        return orders
 
 
 class Trader:
