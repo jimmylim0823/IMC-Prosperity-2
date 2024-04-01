3d128fa0731256b315813867ad86b9579ccaf19c
jimmy <jimmylim0823@yonsei.ac.kr>
2024-04-01 10:21:33 +0900

    First Trader.py

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+
+class Trader:
+
+    def run(self, state: TradingState):
+        result = {}
+        traderData = "SAMPLE"
+        conversions = 0
+        return result, conversions, traderData
